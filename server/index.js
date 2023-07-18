d2b148855754e7081caac38fbfe7642f49bc1574
Yxkana <yxkana52@gmail.com>
2023-07-18 11:20:11 +0200

    trying12

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
 // index.js
-const express = require('express')
+import express from "express"
 
 const app = express()
-const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+app.get('/api',(req,res)=>{
+    res.send(`<h5 style="color:green">
+        Hey Geek! you just deployed serverless express api</h5>`)
 })
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+app.listen(8080,()=>{
+    console.log('Server started at http://localhost:8080')
 })
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
-
-// Export the Express API
-module.exports = app
+module.exports=app
